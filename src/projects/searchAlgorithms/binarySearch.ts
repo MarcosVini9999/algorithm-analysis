bbb308da3ce4a0236d570bc90bac9c3d42e6d884
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2023-09-04 09:15:58 -0300

    feat: binary search

---
+++ src/projects/searchAlgorithms/binarySearch.ts
@@ -0,0 +1,17 @@
+export class binarySearch {
+  constructor() {}
+
+  search(number: number, sequence: number[], start: number, end: number): number {
+    const midIndex = Math.floor((start + end) / 2);
+    if (sequence[midIndex] == number) {
+      return midIndex;
+    }
+    if (start >= end) {
+      return -1;
+    } else if (sequence[midIndex] < number) {
+      return this.search(number, sequence, midIndex + 1, end);
+    } else {
+      return this.search(number, sequence, start, midIndex - 1);
+    }
+  }
+}
